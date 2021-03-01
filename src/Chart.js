3f5db786547f9a13bfc84026ff23af01a7720d52
Rando Rostok <rrostok@malwarebytes.com>
2021-03-01 21:25:46 +0200

    dropdown on legend

--- src/Chart.js
+++ src/Chart.js
@@ -1,10 +1,75 @@
 import React from 'react';
 import { Chart } from 'chart.js';
+import { Menu, MenuItem } from '@material-ui/core';
 
 const MyChart = (props) => {
     const [chart, setChart] = React.useState();
     const chartRef = React.useRef();
     const [data, setData] = React.useState(props.data);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [legend, setLegend] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleEditClick = () => {
+        console.log('OPEN EDIT');
+    };
+
+    const legendMarkup = (chart) => {
+        const updateDataset = (e, index) => {
+            const meta = chart.getDatasetMeta(index);
+
+            // See controller.isDatasetVisible comment
+            meta.hidden = meta.hidden === null? !chart.data.datasets[index].hidden : null;
+
+            const el = document.getElementById('legend').querySelector('li').querySelector('#text');
+
+            if (el.classList.contains("disable-legend")) {
+                el.classList.remove("disable-legend");
+            } else {
+                el.classList.add("disable-legend");
+            }
+            chart.update();
+        };
+
+        return (
+            <div id="legend">
+                {
+                    chart.data.datasets.map((set, i) => {
+                        console.log(set);
+                        return (
+                        <li
+                            id={i}
+                            className="dropdown"
+                            style={{ cursor: 'pointer' }}
+                        >
+                            <span className="dropdown-btn">
+                                <span style={{ 
+                                    display: 'inline-block', 
+                                    backgroundColor: set.borderColor, 
+                                    width: '20px',
+                                    height: '10px',
+                                    marginRight: '5px',
+                                }}></span>
+                                <span id="text">{ set.label }</span>
+                            </span>
+                            <div class="dropdown-content">
+                                <a href="#" onClick={handleEditClick}>Edit</a>
+                                <a href="#" onClick={(e) => updateDataset(e, i)}>Hide</a>
+                            </div>
+                        </li>
+                    )
+                    })
+                }
+            </div>
+        )
+    };
 
     React.useEffect(() => {
         setChart(
@@ -13,13 +78,18 @@ const MyChart = (props) => {
                 data: {
                     datasets: [{
                         data: [],
-                        label: 'Dataset 1',
+                        label: 'Külmamasin, Võimsus',
                         fill: false,
                         borderColor: 'rgb(54, 162, 235)',
                         lineTension: .1,
                       }]
                 },
                 options: {
+                    legend: {
+                        display: false,
+                        position: 'top'
+                    },
+                    legendCallback: legendMarkup,
                     scales: {
                         xAxes: [{
                           type: 'realtime',
@@ -90,11 +160,26 @@ const MyChart = (props) => {
         );
     }, []);
 
+    React.useEffect(() => {
+        if (chart) {
+            setLegend(chart.generateLegend());  
+        }
+    }, [chart]);
+
+    React.useEffect(() => {
+        document.querySelectorAll("#legend li").forEach((item, index) => {
+            item.addEventListener("click", e => handleClick(e, index));
+        });
+    }, [legend])
+
     return (
-        <canvas
-            ref={chartRef}
-            style={{ maxWidth: '900px' }}
-        />
+        <React.Fragment>
+            { legend }
+            <canvas
+                ref={chartRef}
+                style={{ maxWidth: '900px' }}
+            />
+        </React.Fragment>
     );
 };
 
